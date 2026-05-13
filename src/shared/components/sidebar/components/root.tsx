bed4e9533ba43d8abbc71391a8788fbd9a5b1b10
Thiago Brum <thiagobrum222@gmail.com>
2026-05-12 21:18:16 -0300

    🛠️  [fix] (all): fixed bugs

--- src/shared/components/sidebar/components/root.tsx
+++ src/shared/components/sidebar/components/root.tsx
@@ -1,34 +1,36 @@
 import type { ReactNode } from "react";
-import { useLayoutStore } from "@/shared/store/layout/layout.store"
+import { useLayoutStore } from "@/shared/store/layout/layout.store";
 import { motion } from "framer-motion";
+import { isMobile } from "react-device-detect";
 
 export interface RootProps {
-    children: ReactNode
+    children: ReactNode;
 }
 
-export const Root = ({ children }:RootProps) => {
-
+export const Root = ({ children }: RootProps) => {
     const { sideOpen } = useLayoutStore();
+
+    const shouldShowSidebar = isMobile ? sideOpen : true;
 
     return (
         <motion.aside
-            initial={{ opacity: 0, translateX: -200 }}
-            animate={{ opacity: 1, translateX: 0 }}
+            initial={{ opacity: 0 }}
+            animate={{
+                opacity: 1,
+                x: shouldShowSidebar ? 0 : "-100%",
+            }}
             transition={{
                 duration: 0.8,
-                delay: 0.1,
-                ease: "easeInOut",
             }}
-            className={`
-                bg-[--primary-blue-color] dark:bg-[--primary-blue-color-dark]
-                border-r h-full w-full md:max-w-60 z-20 py-8 
-                fixed top-0 left-0 flex
-                flex-col gap-12 border-blue-800 pt-[56px]
-                ${ !sideOpen ? "translate-x-full" : "translate-x-0" }
-                md:translate-x-0
-            `}
-        >
-            { children }
+            className="
+                bg-[--primary-blue-color]
+                dark:bg-[--primary-blue-color-dark]
+                border-r h-full w-full md:max-w-60 z-20 py-8
+                fixed top-0 left-0 flex flex-col gap-12
+                border-blue-800 pt-[56px]
+            "
+            >
+            {children}
         </motion.aside>
-    )
-}
+    );
+};
